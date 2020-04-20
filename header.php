f179c89b0b1d7689d2fb1a69ea8752089185b905
Michael Granil <mila@Michaels-MacBook-Pro.local>
2020-04-20 10:42:50 +0100

    inlcude meta tag description

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Web & Front-End Developer">
     <script src="scripts/jQuery_min_v3.4.1.js" type="text/javascript"></script>
     <script src="scripts/main.js" type="text/javascript"></script>
     <link rel="icon" href="images/favicon.jpg" type="image/gif" sizes="16x16">
